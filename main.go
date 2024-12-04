4d543f396227bfd1ce3ce4779e1c6b55bbdc4b8f
IPampurin <i.pampurin@yandex.ru>
2024-12-04 14:10:50 +0500

    Add division by 0 check. Swimming valid.

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if t.Duration.Hours() == 0 {
+		return 0
+	}
 	return t.distance() / t.Duration.Hours()
 }
 
@@ -164,6 +167,9 @@ type Swimming struct {
 // Это переопределенный метод Calories() из Training.
 func (s Swimming) meanSpeed() float64 {
 	// вставьте ваш код ниже
+	if s.Duration.Hours() == 0 {
+		return 0
+	}
 	return (float64(s.LengthPool) * float64(s.CountPool)) / MInKm / s.Duration.Hours()
 }
 
@@ -180,7 +186,10 @@ func (s Swimming) Calories() float64 {
 // Это переопределенный метод TrainingInfo() из Training.
 func (s Swimming) TrainingInfo() InfoMessage {
 	// вставьте ваш код ниже
-	return s.Training.TrainingInfo()
+	mes := s.Training.TrainingInfo()
+	mes.Distance = (float64(s.LengthPool) * float64(s.CountPool)) / MInKm
+	mes.Speed = s.meanSpeed()
+	return mes
 }
 
 // ReadData возвращает информацию о проведенной тренировке.
